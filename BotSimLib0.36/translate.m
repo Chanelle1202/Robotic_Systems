368b2b2d4b5d8b70b17d0044c8d2601010430b0f
Chanelle Lee <chanelle1202@googlemail.com>
2016-04-12 16:36:30 +0100

    Revert "Chanelle: Particle Filter in new function, copied nav stuff"
    This reverts commit 61aecbaa4b0c832e4d727493bdc6740026cc6f50.

--- BotSimLib0.36/translate.m
+++ BotSimLib0.36/translate.m
@@ -1,7 +1,6 @@
 function [ output ] = translate(pos,transMat )
-% Funtion which takes in a homogenous translatation matrix 
-% and a 2D position vector to perfrom a translation. 
-
+%translate takes a homogenous translatation matrix and 2d position vector
+%and performs a translation on it.
 posHomo = cat(2,pos,ones(size(pos,1),1));
 output = (transMat*posHomo')';
 output(:,3) = [];
