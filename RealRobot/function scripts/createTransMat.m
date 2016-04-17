83b3d5b1963be868a9ac86b373b638e695faf19a
Chanelle Lee <chanelle1202@googlemail.com>
2016-04-17 13:09:56 +0100

    Revert "Revert "updating real robot code to latest version""
    This reverts commit f817c9cd1e27ca8e880c422f7bcff0487f590788.

--- RealRobot/function scripts/createTransMat.m
+++
@@ -1,6 +0,0 @@
-function [ transMat ] = createTransMat( translation )
-% Function which takes a 2D vector input and returns a translation matrix
-% with homogenous co-ordinates.
-
-transMat =[1 0 translation(1);0 1 translation(2);0 0 1];
-end
