ce770c192e9dd6e9df7608dfa3bc2b0748d329e0
zeke.steer <mail@zekesteer.me.uk>
2016-04-17 11:23:07 +0100

    updating real robot code to latest version

--- RealRobot/function scripts/createRotMat.m
+++
@@ -1,6 +0,0 @@
-function [ rotMat ] = createRotMat(angle)
-% Function which takes a 2D vector and returns a rotation matrix with 
-% homogenous co-ordinates.
-
-rotMat =[cos(angle) -sin(angle) 0; sin(angle) cos(angle) 0;0 0 1];
-end
--- RealRobot/function scripts/translate.m
+++
@@ -1,9 +0,0 @@
-function [ output ] = translate(pos,transMat )
-% Funtion which takes in a homogenous translatation matrix 
-% and a 2D position vector to perfrom a translation. 
-
-posHomo = cat(2,pos,ones(size(pos,1),1));
-output = (transMat*posHomo')';
-output(:,3) = [];
-end
-
